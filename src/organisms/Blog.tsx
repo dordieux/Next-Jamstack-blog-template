5d42de5e747bcd7f4a0e59f42d393225cda266f9
dordieux <dordieux@gmail.com>
2021-04-18 16:04:13 +0900

    add blog to top page

---
+++ src/organisms/Blog.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { css } from "@emotion/react";
+
+import Title from "../atoms/home/Title";
+import Subtitle from "../atoms/home/Subtitle";
+import Text from "../atoms/home/Text";
+
+export default function Blog() {
+  return (
+    <section id="blog" css={section}>
+      <div css={container}>
+        <Title>This blog</Title>
+        <Subtitle>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Subtitle>
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat.
+        </Text>
+      </div>
+    </section>
+  );
+}
+
+const section = css`
+  padding: 150px 0 100px;
+  @media screen and (max-width: 900px) {
+    padding-top: 100px;
+  }
+`;
+
+const container = css`
+  width: 75%;
+  margin: 0 auto;
+  @media screen and (max-width: 600px) {
+    width: 85%;
+  }
+`;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import Hero from "../organisms/Hero";
 import About from "../organisms/About";
+import Blog from "../organisms/Blog";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <About />
+      <Blog />
     </>
   );
 }
