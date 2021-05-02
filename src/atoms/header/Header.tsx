c02b548c4bbd41064b039ee53eb7edb502146e98
dordieux <dordieux@gmail.com>
2021-05-03 01:58:19 +0900

    add scroll animation to header

--- src/atoms/header/Header.tsx
+++ src/atoms/header/Header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { css } from "@emotion/react";
 
 type Props = {
@@ -6,7 +6,21 @@ type Props = {
 };
 
 export default function Header(props: Props) {
-  return <header css={header}>{props.children}</header>;
+  const [isTop, setIsTop] = useState(true);
+
+  function onScroll() {
+    const position = window.pageYOffset;
+    position < 80 ? setIsTop(true) : setIsTop(false);
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  const styles = isTop ? [header, transparent] : header;
+
+  return <header css={styles}>{props.children}</header>;
 }
 
 const header = css`
@@ -14,11 +28,15 @@ const header = css`
   width: 100%;
   color: #555;
   background-color: #fff;
-  border-radius: 3px;
+  border-bottom-left-radius: 3px;
+  border-bottom-right-radius: 3px;
   padding: 1rem 0;
   z-index: 10;
   box-shadow: 0 4px 18px 0px rgba(0, 0, 0, 0.12), 0 7px 10px -5px rgba(0, 0, 0, 0.15);
   transition: all 150ms ease 0s;
+`;
+
+const transparent = css`
   background-color: transparent !important;
   box-shadow: none;
   padding-top: 20px;
